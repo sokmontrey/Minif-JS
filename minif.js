e47618c2f7c23c69ca13dfa500ad58af0fb2f65c
SokmontreyTDSB <sokmontrey.sythat@student.tdsb.on.ca>
2022-06-13 20:02:48 -0400

    include class DOM

--- minif.js
+++ minif.js
@@ -1,11 +1,37 @@
-class ComponentManager{
+class DOM{
+	getWithClass(class_name, element=document){
+		return element.getElementsByClassName(class_name);
+	}
+	getWithId(id, element=document){
+		return element.getElementById(id);
+	}
+	getWithAttribute(attr_name, value=null, element=document){
+		const attr = `[${attr_name}${value!==null?`="${value}"`:''}]`;
+		return element.querySelectorAll(attr);
+	}
+	getWithTag(tag_name, element=document){
+		return element.getElementsByTagName(tag_name);
+	}
+
+	hideElement(element){
+		element.style.visibility = 'hidden';
+		element.style.display = 'none';
+	}
+	showElement(element){
+		element.style.visibility = 'visible';
+		element.style.display = 'block';
+	}
+}
+const dom = new DOM();
+
+class Component{
 	type = "component";
 	constructor({type}){
 		this.type = type;
 	}
 
 	_getComponent(){
-		this.type
+		dom.getWithTag
 	}
 
 }
