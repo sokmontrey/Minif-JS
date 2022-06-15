06ff80cd025cf7071127df42e58229b12e92b17e
Sokmontrey <sythatsokmontrey@gmail.com>
2022-06-15 12:13:04 -0400

    sketch out what using the minif would look like

--- minif.js
+++ minif.js
@@ -1,1 +1,27 @@
-class App{}
+class ComponentManager{
+	type = "component";
+	constructor({type}){
+		this.type = type;
+	}
+
+	_getComponent(){
+		this.type
+	}
+
+}
+/*
+class App{
+	constructor(){
+		const data = {
+			1: {
+				a: "a1",
+				b: "b1"
+			},
+			2: {
+				a: "a2",
+				b: "b2"
+			}
+		};
+	}
+}
+*/
